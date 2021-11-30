2f9c6f82dd3afd27ed63a4dc93f395d3daac9e79
ag.thanhz <ag.thanhz@gmail.com>
2021-11-30 20:21:38 +0700

    config passport google oauth20

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const handlebarsEngine = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 // check DB connections
 connectDB();
@@ -20,6 +25,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
 // Template engine Handlebars
 app.engine(
   '.hbs',
@@ -28,11 +38,24 @@ app.engine(
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(PORT, () => {
   console.log(
